1ba28a1bbe3528ac5ad66bdc44b17eb8c8f99965
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2023-01-12 17:57:02 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,21 +4,31 @@
 #include "typewise-alert.h"
 
 TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(20, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(30, 20, 30) == NORMAL);
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(inferBreach(32, 20, 30) == TOO_HIGH);
   REQUIRE(inferBreach(22, 20, 30) == NORMAL);
+  
  
 }
 
 TEST_CASE("Classify the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 35) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20) == NORMAL);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 45) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
   
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 45) == NORMAL);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 20) == NORMAL);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW);
   
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 40) == NORMAL);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 20) == NORMAL);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 45) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
@@ -26,7 +36,7 @@ TEST_CASE("Classify the breach according to limits") {
 }
 
 TEST_CASE("check and alert the controller or mailbox") {
-  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar.PASSIVE_COOLING, 30) == "0xfeed: NORMAL");
+  REQUIRE(checkAndAlert(TO_CONTROLLER, BatteryCharacter.PASSIVE_COOLING, 30) == "0xfeed: NORMAL");
   //REQUIRE(checkAndAlert(TO_CONTROLLER, PASSIVE_COOLING, 45) == "0xfeed: TOO_HIGH");
   //REQUIRE(checkAndAlert(TO_CONTROLLER, PASSIVE_COOLING, -1) == "0xfeed: TOO_LOW");
  
