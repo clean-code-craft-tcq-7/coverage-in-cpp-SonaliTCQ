5b4a2e34e4cf38afbb5315ea26a70a26e04a3a1b
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2023-01-12 10:18:19 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <map>
 
-std::map<CoolingType, Limit> CoolingLim;
+std::map<CoolingType, mapLimit> CoolingLim;
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
 	if (value < lowerLimit) {
