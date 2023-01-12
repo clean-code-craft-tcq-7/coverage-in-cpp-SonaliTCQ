08be19f1cc8bfa52feca2c1f68cef484e9fa6eb6
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2023-01-12 17:32:27 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -7,6 +7,14 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(inferBreach(32, 20, 30) == TOO_HIGH);
   REQUIRE(inferBreach(22, 20, 30) == NORMAL);
+ 
+}
+
+TEST_CASE("Classify the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 45) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
+ 
 }
 
 
